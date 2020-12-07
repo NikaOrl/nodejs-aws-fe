133b5a215298bd0784ba1bd6c608d616505ff7f5
Nika Orlova <veronika_orlova@epam.com>
2020-12-07 22:35:27 +0300

    feat(task-8): add proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://sfuj8ilp8h.execute-api.us-east-1.amazonaws.com/dev",
   import: "https://xreboz193f.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://sfuj8ilp8h.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://nikaorl-cart-api-develop.us-east-1.elasticbeanstalk.com/api"
+  cart: "https://5uuotor4l1.execute-api.us-east-1.amazonaws.com/develop/api"
 };
 
 export default API_PATHS;
