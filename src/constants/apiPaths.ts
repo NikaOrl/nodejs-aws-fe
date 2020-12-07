e1ec7d1009a84998ba2224c50ce0bd8b17031628
Nika Orlova <veronika_orlova@epam.com>
2020-12-07 22:16:18 +0300

    feat(task-8): update api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev"
+  product: "https://sfuj8ilp8h.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://sfuj8ilp8h.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://xreboz193f.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://sfuj8ilp8h.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://nikaorl-cart-api-develop.us-east-1.elasticbeanstalk.com/api"
 };
 
 export default API_PATHS;
